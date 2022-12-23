f25cdeae45040436151f76fc12fc381ae79a4a69
David de Matheu <dademath@microsoft.com>
2022-12-22 17:14:36 -0800

    first commit

---
+++ Chat Widget Function/ChatWidgetFunction/index.ts
@@ -0,0 +1,42 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { CommunicationIdentityClient } from "@azure/communication-identity"
+import { ChatClient } from "@azure/communication-chat";
+import { AzureCommunicationTokenCredential } from "@azure/communication-common";
+
+const connectionString = process.env.ACS_CONNECTION_STRING; //Replace with your connection string
+const endpoint = process.env.ACS_ENDPOINT; //Replace with your endpoint
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    let tokenClient = new CommunicationIdentityClient(connectionString);
+    const user = await tokenClient.createUser();
+    const userToken = await tokenClient.getToken(user, ["chat"]);
+
+    let chatClient =  new ChatClient(endpoint, new AzureCommunicationTokenCredential(userToken.token))
+
+    const createChatThreadRequest = {
+        topic: "Bot Interaction"
+    };
+
+    const createChatThreadOptions = {
+        participants: [
+            {
+                id: { communicationUserId: user.communicationUserId },
+                displayName: 'Local User'
+            }
+        ]
+    };
+
+    const createChatThreadResult = await chatClient.createChatThread(
+        createChatThreadRequest,
+        createChatThreadOptions
+    );
+    
+    const threadId = createChatThreadResult.chatThread.id;
+
+    context.res = {
+        body: { user, userToken, threadId, endpoint}
+    };
+
+};
+
+export default httpTrigger;
